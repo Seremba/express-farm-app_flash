dc1652c8004150c16dac8c9cfda3037cd6b2a734
Seremba <pserembae@gmail.com>
2022-09-14 13:20:24 +0300

    refactor code for flashing messages

--- index.js
+++ index.js
@@ -32,13 +32,19 @@ app.use(flash())
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 //Farm Routes
+app.use((req, res, next) => {
+    res.locals.messages = req.flash('success');
+    next();
+});
+
 app.get('/farms', async (req, res) => {
     const farms = await Farm.find({});
-    res.render('farms/index', {farms, messages: req.flash('success')});
-})
+    res.render('farms/index', {farms});
+});
+
 app.get('/farms/new', (req, res) => {
     res.render('farms/new');
-})
+});
 
 app.get('/farms/:id', async (req, res) => {
     const { id } = req.params
